35c9f9058bda74743091b3459751aeca28bef236
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-08 18:18:18 +0530

    Solutions of Codeforces Problemset.

---
+++ Rating 1000/theatreSquare_1A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class theatreSquare_1A{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a = sc.nextInt();
+        long result = (long) Math.ceil((double) n / a) * (long) Math.ceil((double) m / a);
+        System.out.println(result);
+    }
+    sc.close();
+}
