b2eaab5c65e722f612b18b50a04883cc0deb1922
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-09 14:15:36 +0530

    Added new Solutions to the repo.

---
+++ Rating 1300/registrationSystem_4C.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class registrationSystem_4C {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        HashMap<String, Integer> userMap = new HashMap<>();
+        while(n>0){
+            String s=sc.next();
+            if(!userMap.containsKey(s)){
+                 System.out.println("OK");
+                 userMap.put(s, 1);
+            }
+            else{
+                int count = userMap.get(s);
+                System.out.println(s + count);
+                userMap.put(s, count + 1);
+            }
+            n--;
+        }
+        sc.close();
+    }
+}
