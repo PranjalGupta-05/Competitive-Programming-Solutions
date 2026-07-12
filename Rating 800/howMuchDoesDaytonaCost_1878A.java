8def902a27a2fb6e6c823c4f38053cab43a00957
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-12 18:20:33 +0530

    Updated the code with new code for 1878A.

--- Rating 800/howMuchDoesDaytonaCost_1878A.java
+++ Rating 800/howMuchDoesDaytonaCost_1878A.java
@@ -1,6 +1,37 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class howMuchDoesDaytonaCost {
+// public class howMuchDoesDaytonaCost_1878A {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
+//         while (t > 0) {
+//             int n = sc.nextInt();
+//             int m = sc.nextInt();
+//             int[] arr = new int[n];
+//             for (int i = 0; i < n; i++) {
+//                 arr[i] = sc.nextInt();
+//             }
+//             int k = -1;
+//             for(int i=0;i<n;i++){
+//                 if(arr[i] == m){
+//                     k = i;
+//                     break;
+//                 }
+//             }
+//             if(k!=-1){
+//                 System.out.println("Yes");
+//             }
+//             else{
+//                 System.out.println("No");
+//             }
+//             t--;
+//         }
+//     }
+// }
+
+
+import java.util.Scanner;
+public class howMuchDoesDaytonaCost_1878A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
@@ -8,30 +39,18 @@ public class howMuchDoesDaytonaCost {
             int n = sc.nextInt();
             int m = sc.nextInt();
             int[] arr = new int[n];
+            int cnt = 0;
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
-            }
-            int k = -1;
-            for(int i=0;i<n;i++){
-                if(arr[i] == m){
-                    k = i;
-                    break;
-                }
-            }
-            int cnt=0;
-            for(int i=k;i<n;i++){
                 if(arr[i] == m){
                     cnt++;
                 }
-                else{
-                    cnt--;
-                }
             }
-            if(cnt>=0){
-                System.out.println("YES");
+            if(cnt > 0){
+                System.out.println("Yes");
             }
             else{
-                System.out.println("NO");
+                System.out.println("No");
             }
             t--;
         }
