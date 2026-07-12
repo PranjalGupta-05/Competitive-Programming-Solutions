75819c13db5d0ca2a9e36a4cce1910b61819e7d8
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-12 12:49:05 +0530

    Added Solution of a Problem.

--- Rating 800/spell_Check_1722A.java
+++ Rating 800/spell_Check_1722A.java
@@ -3,27 +3,29 @@ import java.util.Arrays;
 
 public class spell_Check_1722A {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        char[] name = {'T', 'i', 'm', 'u', 'r'};
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        char[] name={'T', 'i', 'm', 'u', 'r'};
         Arrays.sort(name);
         while(t>0){
-            int n = sc.nextInt();
-            String s = sc.next();
+            int n=sc.nextInt();
+            String s=sc.next();
             if(n!=5){
                 System.out.println("NO");
             } 
-            else {
-                char[] arr = s.toCharArray();
+            else{
+                char[] arr=s.toCharArray();
                 Arrays.sort(arr);
                 
-                if (Arrays.equals(name, arr)) {
+                if(Arrays.equals(name, arr)) {
                     System.out.println("YES");
-                } else {
+                } 
+                else{
                     System.out.println("NO");
                 }
             }
             t--;
         }
+        sc.close();
     }
 }
