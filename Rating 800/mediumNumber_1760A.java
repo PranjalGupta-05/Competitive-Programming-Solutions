8112e5c056c760bc0b48855ad5a278647180e486
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-17 23:33:54 +0530

    Added solution of Problem 1760A.

---
+++ Rating 800/mediumNumber_1760A.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class mediumNumber_1760A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int max=Math.max(a, Math.max(b, c));
+            int min=Math.min(a, Math.min(b, c));
+            int sum=a+b+c;
+            int median=sum-max-min;
+            System.out.println(median);
+        }
+    }
+}
