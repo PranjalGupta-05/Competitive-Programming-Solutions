20fc98fb5ce1eff697868d93be4f05704a7f60b9
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-23 14:50:25 +0530

    Added Solution of Problem 451A.

---
+++ Rating 800/gameWithSticks_451A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class gameWithSticks_451A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        if((n*m)%2 == 0){
+            System.out.println("Malvika");
+        } 
+        else{
+            System.out.println("Akshat");
+        }
+        sc.close();
+    }
+}
