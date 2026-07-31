c772e4c05f6da68eb829cf27b508f0828db2ddf0
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-07-31 11:48:58 +0530

    Added Solution of Problem 677A.

--- Rating 800/triple_1669B.java
+++ Rating 800/triple_1669B.java
@@ -1,28 +1,25 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class triple_1669B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- > 0) {
+        
+        while (t-- > 0) {
             int n = sc.nextInt();
-            int[] arr = new int[n];
-            HashMap<Integer, Integer> count = new HashMap<>();
-            for(int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-                count.put(arr[i], count.getOrDefault(arr[i], 0) + 1);
-            }
-            if(n<3){
-                System.out.println(-1);
-                return ;
-            }
-            for(int i = 0; i < n; i++) {
-                if(count.get(arr[i]) >= 3 ) {
-                    System.out.println(arr[i]);
-                    break;
+            int[] count = new int[n + 1];
+            int ans = -1;
+            
+            for (int i = 0; i < n; i++) {
+                int val = sc.nextInt();
+                count[val]++; 
+                if (count[val] >= 3) {
+                    ans = val;
                 }
             }
+            System.out.println(ans);
         }
+        
         sc.close();
     }
 }
---
+++ Rating 800/vanyaAndFence_677A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class vanyaAndFence_677A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        int width = 0;
+        while(n-- > 0){
+            int height = sc.nextInt();
+            if(height > h){
+                width += 2;
+            } else {
+                width += 1;
+            }
+        }
+        System.out.println(width);
+    }
+}
