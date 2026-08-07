71abd6d0bb65748f7170bc2f0e97a70783e94d6b
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-08-07 15:12:55 +0530

    Added Solution of Problem 2009A.

--- Rating 800/Minimize_2009A.java
+++ Rating 800/Minimize_2009A.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
-public class Tram_116A {
+public class Minimize_2009A {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int sum=0;
-        int ans=0;
-        for(int i=0;i<n;i++){
+        while(n-->0){
             int a=sc.nextInt();
             int b=sc.nextInt();
-            sum+=b-a;
-            ans=Math.max(ans,sum);
+            int c=(a+b)/2;
+            System.out.println((c-a)+(b-c));
         }
-        System.out.println(ans);
-        sc.close();
     }
 }
