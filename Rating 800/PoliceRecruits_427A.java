20ac5242791494e41684e1a93efe5395efdbd5fb
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-08-09 14:40:40 +0530

    Added Solution Of Problem 427A.

---
+++ Rating 800/PoliceRecruits_427A.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PoliceRecruits_427A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int recruits=0;
+        int crimes=0;
+        while(n-->0){
+            int x=sc.nextInt();
+            if(x>=0){
+                recruits+=x;
+            }
+            else{
+                if(recruits>0){
+                    recruits--;
+                }
+                else{
+                    crimes++;
+                }
+            }
+        }
+        System.out.println(crimes);
+    }
+}
