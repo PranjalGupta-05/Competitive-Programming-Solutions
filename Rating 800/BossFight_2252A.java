23415367f7a35321bf1b6b6308a6bcb21a0021cd
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-08-18 22:08:28 +0530

    Added SOlution Of Problem 732A.

--- Rating 800/BossFight_2252A.java
+++ Rating 800/BossFight_2252A.java
@@ -4,21 +4,19 @@ public class BossFight_2252A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
-        ArrayList<Integer> arr=new ArrayList<>();
+        
         while(t-- >0){
             int n=sc.nextInt();
+            ArrayList<Integer> arr=new ArrayList<>();
             for(int i=0;i<n;i++){
                 arr.add(sc.nextInt());
             }
-            int ans=arr.get(0);
+            long ans = 0;
             for(int i=1;i<n;i++){
-                if(arr.get(i-1)==arr.get(i)){
-                    ans+=arr.get(i);
-                    break;
+                if (i>=2 && arr.get(i).equals(arr.get(i-1)) && arr.get(i).equals(arr.get(i-2))){
+                    continue; 
                 }
-                else{
-                    ans+=arr.get(i);
-                }
+                ans+=arr.get(i);
             }
             arr.clear();
             System.out.println(ans);
---
+++ Rating 800/BuyaShovel_732A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class BuyaShovel_732A {
+    public static int finder(int k, int r){
+        int ans=0;
+        for(int i=1;i<=10;i++){
+            ans=k*i;
+            if(ans%10==0 || ans%10==r){
+                return i;
+            }
+        }
+        return -1; // This line should never be reached based on the problem constraints
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int r=sc.nextInt();
+        System.out.println(finder(k,r));
+        sc.close();
+    }
+}
