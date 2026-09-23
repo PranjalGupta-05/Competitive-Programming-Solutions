bba98f46f918d4feb9af830bd30568aff26625d9
PranjalGupta-05 <pranjaljhs2005@gmail.com>
2026-09-23 19:49:40 +0530

    Added Solution Of Problem 1374A.

--- Rating 800/RequiredRemainder_1374A.java
+++ Rating 800/RequiredRemainder_1374A.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
-public class MinutesBeforetheNewYear_1283A {
+public class RequiredRemainder_1374A {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t-- >0){
-            int h=sc.nextInt();
-            int m=sc.nextInt();
-            System.out.println((23-h)*60+(60-m));
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int n=sc.nextInt();
+            System.out.println((n/b)*b+a);
         }
         sc.close();
     }
